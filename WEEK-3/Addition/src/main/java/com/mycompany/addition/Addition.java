a1f88b1570edce75915c447ed7709301e474be30
MOHDFAISAL <mfaisal731072@gmail.com>
2026-08-13 20:42:09 +0530

    Add files via upload

---
+++ WEEK-3/Addition/src/main/java/com/mycompany/addition/Addition.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.addition;
+
+
+public class Addition {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        
+        System.out.println("addition+"+(a+b));
+    }
+}
