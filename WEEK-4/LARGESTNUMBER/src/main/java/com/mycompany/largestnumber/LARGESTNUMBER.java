797fddd589854fc089ff04323697d23589c48c7f
MOHDFAISAL <mfaisal731072@gmail.com>
2026-08-18 20:44:19 +0530

    Add files via upload

---
+++ WEEK-4/LARGESTNUMBER/src/main/java/com/mycompany/largestnumber/LARGESTNUMBER.java
@@ -0,0 +1,20 @@
+
+
+package com.mycompany.largestnumber;
+
+public class LARGESTNUMBER {
+
+    public static void main(String[] args) {
+        int a=58;
+        int b=69;
+        int c=98;
+        if(a>b){
+        System.out.println("a is largest number"); }
+        else if(b>c){
+            System.out.println("b is largest number");}
+        else {
+            System.out.println("c is largeset nummber");
+             }
+        }
+    }
+
---
+++ WEEK-4/ODDANDEVEN/src/main/java/com/mycompany/oddandeven/ODDANDEVEN.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.oddandeven;
+
+public class ODDANDEVEN {
+
+    public static void main(String[] args) {
+        int a=7;
+        if (a%2==0){
+        System.out.println("EVEN");
+        }
+        else {
+            System.out.println("ODD");
+            
+        }
+    }
+}
